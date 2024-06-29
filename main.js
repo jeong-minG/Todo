8d89eb14f74fefec2ee867d24d0334d5f7993e95
jeong-minG <ika818488@gmail.com>
2024-06-29 15:29:15 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// 유저가 값을 입력하고 +버튼을 누르면 할 일을 추가할 수 있다
+// 각 할 일에 삭제와 체크버튼이 있다
+// 삭제버튼을 클릭하면 할 일이 리스트에서 삭제된다
+// 끝난 할일은 되돌리기 버튼을 클릭하면 다시 되돌릴 수 있다
+// 탭을 이용해 아이템들 싱테별로 나누어서 볼 수 있다
+// 모바일 버전에서도 확인할 수 있는 반응형 웹이다
+
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let taskList = [];
+
+addButton.addEventListener("click",addTask);
+
+function addTask(){
+    let taskContent = taskInput.value;
+    taskList.push(taskContent);
+    render();
+}
+
+function render(){
+    let resultHtml = ``;
+    for(let i=0;i<taskList.length;i++){
+        resultHtml += `<div class="task">
+                <div>${taskList[i]}</div>
+                <div>
+                    <button>Check</button>
+                    <button>Delete</button>
+                </div>
+                </div>`;
+    }
+    document.getElementById("task-board").innerHTML = resultHtml;
+}
+
+// console.log(addButton);
